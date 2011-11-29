ee8a50c81cb4ef503e5499b79f6ce43ed7e10e10
Alex K. Tang <alexktang@gmail.com>
2011-11-28 21:02:33 -0600

    Removed debug prints

--- src/main/java/collabstream/streaming/RatingsBlock.java
+++ src/main/java/collabstream/streaming/RatingsBlock.java
@@ -40,12 +40,10 @@ public class RatingsBlock {
 
 	public static class Serialization implements ISerialization<RatingsBlock> {
 		public boolean accept(Class c) {
-			System.out.println("######## RatingsBlockSerialization.accept");
 			return RatingsBlock.class.isAssignableFrom(c);
 		}
 		
 		public void serialize(RatingsBlock block, DataOutputStream out) throws IOException {
-			System.out.println("######## RatingsBlockSerialization.serialize");
 			if (block == null || block.examples == null) return;
 			out.writeInt(block.examples.size());
 			
@@ -56,7 +54,6 @@ public class RatingsBlock {
 		}
 		
 		public RatingsBlock deserialize(DataInputStream in) throws IOException {
-			System.out.println("######## RatingsBlockSerialization.deserialize");
 			int numExs = in.readInt();
 			RatingsBlock block = new RatingsBlock(numExs);
 			
