2323ccd2a37d74124d309ec4a9d9aaad91bf54a3
Alex K. Tang <alexktang@gmail.com>
2011-11-28 21:56:19 -0600

    Changed TrainingExample to implement Serializable again. I thought I got it to work without this, but apparently not. Why does TrainingExample need to implement Serializable when I am writing my own custom serialization? Debug prints confirm that the custom serialization is being called.

--- src/main/java/collabstream/streaming/TrainingExample.java
+++ src/main/java/collabstream/streaming/TrainingExample.java
@@ -3,10 +3,11 @@ package collabstream.streaming;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.Serializable;
 
 import backtype.storm.serialization.ISerialization;
 
-public class TrainingExample {
+public class TrainingExample implements Serializable {
 	public int userId, itemId;
 	public float rating;
 	
